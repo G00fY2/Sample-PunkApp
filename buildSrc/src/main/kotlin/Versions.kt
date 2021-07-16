a7cd3425ee9964da6e03671422778cd7dc76b6c9
Thomas Wirth <thomas-w-@web.de>
2021-07-16 11:38:30 +0200

    Update target and compile sdk to 31

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
 
   const val androidMinSdk = 21
-  const val androidCompileSdk = 30
-  const val androidTargetSdk = 30
+  const val androidCompileSdk = 31
+  const val androidTargetSdk = 31
 
   const val androidBuildTools = "31.0.0"
   const val androidGradle = "7.1.0-alpha03"
