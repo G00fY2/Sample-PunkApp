8f83a97deedfb4c2febf0711bd2e1bb29fb4602b
Thomas Wirth <thomas-w-@web.de>
2021-07-22 10:00:13 +0200

    Update Android Studio and deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
   const val androidTargetSdk = 31
 
   const val androidBuildTools = "31.0.0"
-  const val androidGradle = "7.1.0-alpha03"
+  const val androidGradle = "7.1.0-alpha04"
   const val kotlin = "1.5.10"
 
   const val gradleVersions = "0.39.0"
@@ -16,7 +16,7 @@ object Versions {
 
   const val compose = "1.0.0-rc02"
   const val activityCompose = "1.3.0-rc02"
-  const val composeNavigation = "2.4.0-alpha04"
+  const val composeNavigation = "2.4.0-alpha05"
 
   const val materialDesign = "1.5.0-alpha01"
   const val accompanist = "0.14.0"
