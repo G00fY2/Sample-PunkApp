cf4e4c0489ca3c5231fed95c6b86904cde07660b
Thomas Wirth <thomas-w-@web.de>
2021-08-10 02:45:00 +0200

    Update AGP and deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
   const val androidTargetSdk = 31
 
   const val androidBuildTools = "31.0.0"
-  const val androidGradle = "7.1.0-alpha06"
+  const val androidGradle = "7.1.0-alpha07"
   const val kotlin = "1.5.21"
 
   const val gradleVersions = "0.39.0"
@@ -19,7 +19,7 @@ object Versions {
   const val composeNavigation = "2.4.0-alpha06"
 
   const val materialDesign = "1.5.0-alpha01"
-  const val accompanist = "0.16.0"
+  const val accompanist = "0.16.1"
 
   const val coil = "1.3.2"
 
