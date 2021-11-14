9eaa84a87fac890b7a4839ed81fc0b02473c44cf
Thomas Wirth <thomas-w-@web.de>
2021-11-14 23:23:21 +0100

    Update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,21 +11,21 @@ object Versions {
   const val gradleVersions = "0.39.0"
 
   const val coroutines = "1.5.2"
-  const val kotlinxSerialization = "1.3.0"
+  const val kotlinxSerialization = "1.3.1"
   const val ktor = "1.6.5"
 
   const val compose = "1.1.0-beta02"
   const val activityCompose = "1.4.0"
   const val composeNavigation = "2.4.0-beta02"
 
-  const val materialDesign = "1.5.0-alpha05"
+  const val materialDesign = "1.5.0-beta01"
   const val accompanist = "0.21.2-beta"
 
-  const val coil = "2.0.0-alpha02"
+  const val coil = "2.0.0-alpha03"
 
   const val timber = "5.0.1"
 
-  const val daggerHilt = "2.40"
+  const val daggerHilt = "2.40.1"
   const val androidHiltNavigationCompose = "1.0.0-alpha03"
 
   const val detekt = "1.19.0-RC1"
