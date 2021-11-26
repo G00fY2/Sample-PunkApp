367b3fb58a8b447d59864b28295496f26cf2a64c
Thomas Wirth <thomas-w-@web.de>
2021-11-26 19:08:48 +0100

    Update deps, switch to AS beta

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
   const val androidTargetSdk = 31
 
   const val androidBuildTools = "31.0.0"
-  const val androidGradle = "7.2.0-alpha04"
+  const val androidGradle = "7.1.0-beta04"
   const val kotlin = "1.5.31"
 
   const val gradleVersions = "0.39.0"
@@ -14,19 +14,19 @@ object Versions {
   const val kotlinxSerialization = "1.3.1"
   const val ktor = "1.6.5"
 
-  const val compose = "1.1.0-beta02"
+  const val compose = "1.1.0-beta03"
   const val activityCompose = "1.4.0"
   const val composeNavigation = "2.4.0-beta02"
 
   const val materialDesign = "1.5.0-beta01"
-  const val accompanist = "0.21.2-beta"
+  const val accompanist = "0.21.3-beta"
 
-  const val coil = "2.0.0-alpha03"
+  const val coil = "2.0.0-alpha04"
 
   const val timber = "5.0.1"
 
-  const val daggerHilt = "2.40.1"
-  const val androidHiltNavigationCompose = "1.0.0-alpha03"
+  const val daggerHilt = "2.40.2"
+  const val androidHiltNavigationCompose = "1.0.0-beta01"
 
-  const val detekt = "1.19.0-RC1"
+  const val detekt = "1.19.0-RC2"
 }
