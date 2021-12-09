29139893b86ff15d190a855fad663b63bd6df146
Thomas Wirth <thomas-w-@web.de>
2021-12-10 00:26:26 +0100

    Update dependencies and target sdk

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,32 +1,32 @@
 object Versions {
 
   const val androidMinSdk = 21
-  const val androidCompileSdk = 31
-  const val androidTargetSdk = 31
+  const val androidCompileSdk = 32
+  const val androidTargetSdk = 32
 
-  const val androidBuildTools = "31.0.0"
+  const val androidBuildTools = "32.0.0"
   const val androidGradle = "7.1.0-beta04"
-  const val kotlin = "1.5.31"
+  const val kotlin = "1.6.0"
 
   const val gradleVersions = "0.39.0"
 
-  const val coroutines = "1.5.2"
+  const val coroutines = "1.6.0-RC"
   const val kotlinxSerialization = "1.3.1"
-  const val ktor = "1.6.5"
+  const val ktor = "1.6.7"
 
-  const val compose = "1.1.0-beta03"
+  const val compose = "1.1.0-beta04"
   const val activityCompose = "1.4.0"
   const val composeNavigation = "2.4.0-beta02"
 
-  const val materialDesign = "1.5.0-beta01"
-  const val accompanist = "0.21.3-beta"
+  const val materialDesign = "1.6.0-alpha01"
+  const val accompanist = "0.21.4-beta"
 
-  const val coil = "2.0.0-alpha04"
+  const val coil = "2.0.0-alpha05"
 
   const val timber = "5.0.1"
 
-  const val daggerHilt = "2.40.2"
+  const val daggerHilt = "2.40.5"
   const val androidHiltNavigationCompose = "1.0.0-beta01"
 
-  const val detekt = "1.19.0-RC2"
+  const val detekt = "1.19.0"
 }
