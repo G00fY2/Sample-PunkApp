754e184b3c8433b65dd6b2af57b24e26af1b1203
Thomas Wirth <thomas-w-@web.de>
2021-12-10 00:43:44 +0100

    Cleanup settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 include("app")
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     google()
     mavenCentral()
@@ -16,9 +15,6 @@ pluginManagement {
   resolutionStrategy {
     eachPlugin {
       when (requested.id.id) {
-        "com.android.application", "com.android.library" -> {
-          useModule("com.android.tools.build:gradle:${requested.version}")
-        }
         "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
       }
     }
