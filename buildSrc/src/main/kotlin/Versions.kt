5e970a5533b642d39acc7416e81d0cbde664a512
Thomas Wirth <thomas-w-@web.de>
2022-07-24 23:24:08 +0200

    Update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,28 +5,28 @@ object Versions {
   const val androidTargetSdk = 33
 
   const val androidBuildTools = "33.0.0"
-  const val androidGradle = "7.4.0-alpha07"
-  const val kotlin = "1.7.0"
+  const val androidGradle = "7.4.0-alpha08"
+  const val kotlin = "1.7.10"
 
   const val gradleVersions = "0.42.0"
 
-  const val coroutines = "1.6.3"
-  const val kotlinxSerialization = "1.3.3"
+  const val coroutines = "1.6.4"
+  const val kotlinxSerialization = "1.4.0-RC"
   const val ktor = "2.0.3"
 
-  const val composeCompiler = "1.2.0"
+  const val composeCompiler = "1.3.0-beta01"
   const val compose = "1.3.0-alpha01"
   const val activityCompose = "1.6.0-alpha05"
   const val composeNavigation = "2.5.0"
 
-  const val materialDesign = "1.7.0-alpha02"
+  const val materialDesign = "1.7.0-alpha03"
 
   const val coil = "2.1.0"
 
   const val timber = "5.0.1"
 
-  const val daggerHilt = "2.42"
+  const val daggerHilt = "2.43"
   const val androidHiltNavigationCompose = "1.0.0"
 
-  const val detekt = "1.21.0-RC1"
+  const val detekt = "1.21.0"
 }
