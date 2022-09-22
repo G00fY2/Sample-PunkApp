d4efcc53ca539a4a448feb1a987cd3d2918ac0b6
Thomas Wirth <thomas-w-@web.de>
2022-09-22 21:55:50 +0200

    Optimize list and item composables

--- app/src/main/kotlin/com/g00fy2/punkapp/ui/beers/BeerCard.kt
+++ app/src/main/kotlin/com/g00fy2/punkapp/ui/beers/BeerCard.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,31 +22,48 @@ import com.g00fy2.punkapp.ui.theme.PunkAppTheme
 import com.g00fy2.punkapp.ui.theme.Purple200
 
 @Composable
-fun BeerCard(beer: Beer) {
-  Row(modifier = Modifier.padding(all = 8.dp)) {
+fun BeerCard(beer: Beer, modifier: Modifier = Modifier) {
+  Row(modifier = modifier) {
     AsyncImage(
       model = beer.imageUrl,
       contentDescription = null,
       modifier = Modifier
-        .size(128.dp)
+        .weight(1f)
+        .aspectRatio(1f)
         .clip(CircleShape)
         .background(color = Purple200)
+        .padding(8.dp)
     )
 
     Spacer(modifier = Modifier.width(8.dp))
 
-    Column {
-      Text(text = beer.name)
+    Column(modifier = Modifier.weight(1.5f)) {
+      Text(
+        text = beer.name,
+        style = MaterialTheme.typography.h6,
+      )
       Spacer(modifier = Modifier.height(4.dp))
-      Text(text = beer.description)
+      Text(
+        text = beer.description,
+        style = MaterialTheme.typography.body2
+      )
     }
   }
 }
 
-@Preview
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 fun PreviewMessageCard() {
   PunkAppTheme {
-    BeerCard(Beer(0, "Tasty Beer", "", "", "Description", ""))
+    BeerCard(
+      Beer(
+        id = 0,
+        name = "Tasty Beer",
+        tagline = "",
+        firstBrewed = "",
+        description = "Description",
+        imageUrl = ""
+      )
+    )
   }
 }
--- app/src/main/kotlin/com/g00fy2/punkapp/ui/beers/Beers.kt
+++ app/src/main/kotlin/com/g00fy2/punkapp/ui/beers/Beers.kt
@@ -1,20 +1,27 @@
 package com.g00fy2.punkapp.ui.beers
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.g00fy2.punkapp.model.entities.domain.Beer
 import com.g00fy2.punkapp.ui.main.MainViewModel
 
 @Composable
-fun Beers(viewModel: MainViewModel) {
+fun Beers(viewModel: MainViewModel, modifier: Modifier = Modifier) {
+  val beers: List<Beer> by viewModel.beerList.collectAsState()
 
-  val beers: List<Beer> by viewModel.beerList.collectAsState(listOf())
-
-  LazyColumn {
-    items(beers) { beer ->
+  LazyColumn(
+    modifier = modifier,
+    contentPadding = PaddingValues(8.dp),
+    verticalArrangement = Arrangement.spacedBy(16.dp),
+  ) {
+    items(items = beers, key = { it.id }) { beer ->
       BeerCard(beer)
     }
   }
